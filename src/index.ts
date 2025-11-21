fb82c2efb7c9de4f916701e80513c7655612c3ad
Naitive13 <hei.titan.2@gmail.com>
2025-11-21 15:43:25 +0300

    test: use console.log to try some methods

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Wallet } from "./Wallet.ts";
-import { Size } from "./Size.ts";
 
-const myWallet: Wallet = new Wallet("blue", Size.LARGE);
+const myWallet: Wallet = new Wallet("blue", "SMALL");
 
 myWallet.addVola(200_000);
 console.log(myWallet.checkVola());
+myWallet.getVola(300);
+console.log(myWallet.checkVola());
+
+console.log(myWallet.isOpen);
+console.log(myWallet.isLost);
